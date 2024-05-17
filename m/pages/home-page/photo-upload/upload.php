3bf7595b95cccc8eea88d962fcedb508d36b60d9
misqosos <162117367+misqosos@users.noreply.github.com>
2024-05-17 14:35:20 +0200

    Update upload.php

--- m/pages/home-page/photo-upload/upload.php
+++ m/pages/home-page/photo-upload/upload.php
@@ -7,6 +7,6 @@
 
 <?php 
     $actionPath = "/m/home";
-    $targetDirPath = "../m/pages/home-page/photo-upload/photos/";
+    $targetDirPath = "../pages/home-page/photo-upload/photos/";
     include("../pages/home-page/photo-upload/upload-wo-css.php"); 
 ?>
