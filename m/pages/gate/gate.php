0422425a50384f6863881af7a52531a816c8261a
misqosos <michaldrotar14@gmail.com>
2024-05-27 08:25:33 +0200

    heslo

--- m/pages/gate/gate.php
+++ m/pages/gate/gate.php
@@ -60,6 +60,7 @@
 <div class="gate-view">
     <img src="../assets/images/frame.png" alt="ram" class="ram">
     <form action="/m/index.php" method="post" style="text-align: center; position: relative;">
+        <p>Heslo sa nachádza v životopise</p>
         <input type="password" name="pass"><br>
         <input type="hidden" name="uri" value="<?php echo $_SERVER['REQUEST_URI'] ?>">
         <button type="submit" class="submit-button">Potvrdiť</button>
