7a8881910c10ed53d9edc88aa50bf9e7359da865
misqosos <michaldrotar14@gmail.com>
2024-05-28 11:20:04 +0200

    meilik

--- pages/motivation-page/motivation-wo-css.php
+++ pages/motivation-page/motivation-wo-css.php
@@ -3,6 +3,7 @@
 <div style="top: 0; position: relative;">
     <?php if(isset($_POST["companyName"]) && !empty($_POST["companyName"])) : ?>
         <?php noteVisit($_POST["companyName"]); ?>
+        <?php mail("michaldrotar14@gmail.com", $_POST["companyName"]." si pozreli motivak", "predmet") ?>
         <div style="text-align: center;">
             <div class="main-title">MOTIVÁCIA&nbsp;&nbsp; MICHALA&nbsp;&nbsp; NASTÚPIŤ&nbsp;&nbsp; DO&nbsp;&nbsp; <label style="font-weight: bolder;"><?php echo strtoupper($_POST["companyName"]) ?></label>&nbsp;&nbsp; AKO&nbsp;&nbsp; UCHO</div>
             <p class="story-text">
