a16ef2cba657b7412878e5bb0b4a900267ffeffc
misqosos <michaldrotar14@gmail.com>
2024-10-24 13:29:51 +0200

    cas

--- pages/gate/gate.php
+++ pages/gate/gate.php
@@ -8,7 +8,7 @@
 <div class="gate-view">
     <img src="assets/images/frame.png" alt="ram" class="ram">
     <form action="<?php echo $_SERVER['REQUEST_URI'] ?>" method="post" style="text-align: center; position: relative;">
-        <p>Heslo sa nachádza v životopise</p>
+        <p>Heslo = spolocnost</p>
         <input type="password" name="pass"><br>
         <?php if(!$access && isset($_POST["pass"])) : ?>
             <label style="color: red;"><i class="fa fa-exclamation-circle" aria-hidden="true"></i>&nbsp;Nesprávne heslo, skúste znova</label><br>
